da5bb2d10bd22dd653198aee28be41486e69fb4d
AlyAhmed22 <AlyAhmrd2017@gmail.com>
2025-01-11 23:55:33 +0200

    improve

--- helper.py
+++ helper.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Load Data
 @st.cache_data
 def load_data():
-    df = pd.read_csv('covid19app/WHO-COVID-19-global-daily-data (1).csv')
+    df = pd.read_csv('WHO-COVID-19-global-daily-data (1).csv')
     df['Date_reported'] = pd.to_datetime(df['Date_reported'])
     return df
 
